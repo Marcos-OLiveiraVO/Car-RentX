6423d11c644c1d3a619b6023460b87060564387b
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-02 00:29:35 -0300

    fix: debugger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "its working" }).send();
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  res.send(name);
+});
 
 app.listen(PORT, () => console.log("Server is Running"));
