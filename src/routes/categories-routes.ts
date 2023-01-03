9a0c2d6ed81f72fbd53d70716a6abfa5aef488c0
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-03 01:07:43 -0300

    refactor: categories routes types

--- src/routes/categories-routes.ts
+++ src/routes/categories-routes.ts
@@ -1,21 +1,25 @@
 import { Router } from "express";
 import { v4 as uuid } from "uuid";
+import { Categories } from "../models/Category-model";
 
 const categoriesRouters = Router();
-const categories = [];
+const categories: Categories[] = [];
 
 categoriesRouters.post("/", (req, res) => {
   const { description, name } = req.body;
 
-  const category = {
+  const category = new Categories();
+
+  Object.assign(category, {
     id: uuid(),
     name,
     description,
-  };
+    created_At: new Date(),
+  });
 
   categories.push(category);
 
-  res.status(201).send();
+  res.status(201).json({ category });
 });
 
 export { categoriesRouters };
