f35e5fe57fea1f6d82ef9a3b9761473a446b5ebd
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-04 01:04:11 -0300

    refactor: categories repository implement

--- src/routes/categories-routes.ts
+++ src/routes/categories-routes.ts
@@ -1,25 +1,15 @@
 import { Router } from "express";
-import { v4 as uuid } from "uuid";
-import { Categories } from "../models/Category-model";
+import { CategoriesRepository } from "../repositories/categoriesRepository";
 
 const categoriesRouters = Router();
-const categories: Categories[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRouters.post("/", (req, res) => {
   const { description, name } = req.body;
 
-  const category = new Categories();
+  categoriesRepository.create({ description, name });
 
-  Object.assign(category, {
-    id: uuid(),
-    name,
-    description,
-    created_At: new Date(),
-  });
-
-  categories.push(category);
-
-  res.status(201).json({ category });
+  res.status(201).send();
 });
 
 export { categoriesRouters };
