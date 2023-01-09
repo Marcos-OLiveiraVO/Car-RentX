b93159aac93678bf701dd2d71d9d2521e79adfc3
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-09 10:54:30 -0300

    refactor: modules cars implemented

---
+++ src/modules/cars/repositories/categoriesRepository.ts
@@ -0,0 +1,36 @@
+import { Category } from "../models/Category-model";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_At: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+}
+
+export { CategoriesRepository };
