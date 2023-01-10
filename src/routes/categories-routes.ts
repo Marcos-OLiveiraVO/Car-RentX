3362347e13ad8ef53f28086f9423d82a4217a3c9
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-10 12:22:59 -0300

    feat: import csv multer

--- src/routes/categories-routes.ts
+++ src/routes/categories-routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
 const categoriesRouters = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRouters.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -10,6 +15,14 @@ categoriesRouters.post("/", (req, res) => {
 
 categoriesRouters.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRouters.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+
+  console.log(file);
+
+  return res.send();
 });
 
 export { categoriesRouters };
