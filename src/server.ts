e44ad45b7a4ff2f2b4ce76498a4eafcf6788fcb3
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-17 15:51:45 -0300

    refactor: server container dependency

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import "reflect-metadata";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerFile from "../swagger.json";
-import { router } from "./routes";
 
 import "./database/data-source";
+
+import "./shared/container";
+
+import { router } from "./routes";
+import swaggerFile from "../swagger.json";
 
 const app = express();
 const PORT = 3000;
