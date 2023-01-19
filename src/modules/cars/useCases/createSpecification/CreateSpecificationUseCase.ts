121205c4132f876318710cd371bc684142d9c093
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-19 11:39:36 -0300

    refactor: Spefication UseCases

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -12,15 +12,16 @@ class CreateSpecificationUseCase {
     @inject("SpecificationRepository")
     private specificationsRepository: ISpecificationRepository
   ) {}
-  execute({ name, description }: IRequest): void {
+
+  async execute({ name, description }: IRequest): Promise<void> {
     const specificationAlreadyExists =
-      this.specificationsRepository.findByName(name);
+      await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
       throw new Error("Specification Already Exists!");
     }
 
-    this.specificationsRepository.create({ name, description });
+    await this.specificationsRepository.create({ name, description });
   }
 }
 
