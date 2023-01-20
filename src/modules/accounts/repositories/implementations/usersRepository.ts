c87503daf754b63cd080a0ea3b5894ed58204a36
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-20 13:00:30 -0300

    refactor: user repositories

---
+++ src/modules/accounts/repositories/implementations/usersRepository.ts
@@ -0,0 +1,33 @@
+import { Repository } from "typeorm";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUserRepository } from "../IUserRepository";
+import { User } from "../../entities/User";
+import dataSource from "../../../../database/data-source";
+
+class UsersRepository implements IUserRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(User);
+  }
+
+  async create({
+    name,
+    username,
+    email,
+    password,
+    driver_licence,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_licence,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
