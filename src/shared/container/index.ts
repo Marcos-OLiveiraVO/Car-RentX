aa82c3bcd9954884c411de889499f8d802b16ca6
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-20 15:51:47 -0300

    User container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,10 @@
 import { container } from "tsyringe";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/usersRepository";
+import { IUserRepository } from "../../modules/accounts/repositories/IUserRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/categoriesRepository";
 import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
-import { IUserRepository } from "../../modules/accounts/repositories/IUserRepository";
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/usersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
