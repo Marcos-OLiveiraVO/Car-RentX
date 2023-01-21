aa17279a7675d35f7fee9e66e285fddd3432b004
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-21 20:00:46 -0300

    refactor: username prop schema removed

--- src/modules/accounts/repositories/implementations/usersRepository.ts
+++ src/modules/accounts/repositories/implementations/usersRepository.ts
@@ -13,14 +13,12 @@ class UsersRepository implements IUserRepository {
 
   async create({
     name,
-    username,
     email,
     password,
     driver_licence,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       email,
       password,
       driver_licence,
