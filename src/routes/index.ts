d7a78b853f794ecb84d8770079857c5be21e27e8
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-01-22 12:15:37 -0300

    refactor: user session routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { categoriesRouters } from "./categories-routes";
 import { specificationRouters } from "./specification-routes";
 import { usersRouters } from "./users-routes";
+import { authenticateRouter } from "./authenticate-routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRouters);
 router.use("/specifications", specificationRouters);
 router.use("/users", usersRouters);
+router.use(authenticateRouter);
 
 export { router };
