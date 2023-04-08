3839916e5687afdd89b99529e08b2edf288cd0a0
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-04-08 00:56:04 -0300

    feat: seed admin user

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,20 @@
+import dataSource from "../data-source";
+
+import { v4 as uuidV4 } from "uuid";
+import { hashSync } from "bcrypt";
+
+async function create() {
+  const id = uuidV4();
+  const password = hashSync("admin", 8);
+
+  await dataSource.initialize();
+
+  await dataSource.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, "driver_licence")
+    values('${id}', 'admin', 'admin@mail.com', '${password}', true, 'now()', 'XXX-XXX')`
+  );
+
+  await dataSource.destroy();
+}
+
+create().then(() => console.log("User admin created !"));
