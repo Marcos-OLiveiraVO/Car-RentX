214bb32f54c1837ccb6dde1ba032b42ff08dfd72
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-05-23 23:00:32 -0300

    feat: add S3StorageProvider

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,50 @@
+import upload from "@config/upload";
+import fs from "fs";
+import { IStorageProvider } from "../IStorageProvider";
+
+import { resolve } from "path";
+import { S3 } from "aws-sdk";
+import mime from "mime";
+
+class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new S3({
+      region: process.env.AWS_BUCKET_REGION,
+    });
+  }
+
+  async save(file: string, folder: string): Promise<string> {
+    const originaName = resolve(upload.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originaName);
+
+    const ContentType = mime.getType(originaName);
+
+    await this.client
+      .putObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+        ACL: "public-read",
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originaName);
+
+    return file;
+  }
+
+  async delete(file: string, folder: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export { S3StorageProvider };
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayJsDateProvider);
 
@@ -13,7 +14,12 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 );
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  diskStorage[process.env.DISK]
 );
