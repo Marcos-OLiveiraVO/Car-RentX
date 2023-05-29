85700de57aeab3b63ba44017e1bd72ed145ad52c
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-05-28 23:07:41 -0300

    refactor: container categoriesRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,8 @@ import { container } from "tsyringe";
 import "@shared/container/providers";
 
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
-import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/categoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/categoriesRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { IUserRepository } from "@modules/accounts/repositories/IUserRepository";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/usersRepository";
