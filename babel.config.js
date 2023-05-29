4adc1fcd75d271bb34c481b23596b2068bccb7c3
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-05-29 00:52:57 -0300

    build: babel refactor

--- babel.config.js
+++ babel.config.js
@@ -18,5 +18,6 @@ module.exports = {
     "babel-plugin-transform-typescript-metadata",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-transform-runtime",
   ],
 };
